cbdd3568370ed03b36bab9b2a140365d86175958
jo-minjun <alswns9288@gmail.com>
2025-12-20 13:47:01 +0900

    [inflearn] 정렬 문제 풀이

---
+++ src/main/java/me/minjun/inflearn/sortingSearching/BubbleSort.java
@@ -0,0 +1,54 @@
+package me.minjun.inflearn.sortingSearching;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * 설명
+ *
+ * <p>N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+ *
+ * <p>정렬하는 방법은 버블정렬입니다.
+ *
+ * <p>입력
+ *
+ * <p>첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다.
+ *
+ * <p>두 번째 줄에 N개의 자연수가 공백을 사이에 두고 입력됩니다. 각 자연수는 정수형 범위 안에 있습니다.
+ *
+ * <p>출력
+ *
+ * <p>오름차순으로 정렬된 수열을 출력합니다.
+ */
+public class BubbleSort {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int n = scanner.nextInt();
+    scanner.nextLine();
+    List<Integer> input =
+        Arrays.stream(scanner.nextLine().split(" "))
+            .map(Integer::valueOf)
+            .collect(Collectors.toList());
+
+    System.out.println(
+        solution(n, input).stream().map(String::valueOf).collect(Collectors.joining(" ")));
+  }
+
+  public static List<Integer> solution(int n, List<Integer> input) {
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (input.get(j) > input.get(j + 1)) {
+          int tmp = input.get(j);
+          input.set(j, input.get(j + 1));
+          input.set(j + 1, tmp);
+        }
+      }
+    }
+
+    return input;
+  }
+}
